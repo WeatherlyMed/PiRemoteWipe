71cf5e85177d6da04f28fb8ff9508c6d3bb7db02
WeatherlyMed <163483028+WeatherlyMed@users.noreply.github.com>
2024-08-19 17:18:40 -0400

    Create relayt.py

---
+++ relayt.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+relay_pin = 17
+GPIO.setup(relay_pin, GPIO.OUT)
+
+def trigger_relay():
+    GPIO.output(relay_pin, GPIO.HIGH)
+    time.sleep(60)
+    GPIO.output(relay_pin, GPIO.LOW)
+
+try:
+    while True:
+        trigger_relay()
+        print("killed")
+        time.sleep(5)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
